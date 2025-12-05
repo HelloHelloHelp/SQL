7a80ee9da34131745748671840e266a9cf59b23e
Zamzam Ali <ZAAL@praktikcenter.lan>
2025-12-05 10:15:56 +0100

    try to movie aml to ml

--- Pages/LFMAS/LFM.cshtml.cs
+++ Pages/LFMAS/LFM.cshtml.cs
@@ -5,10 +5,14 @@ using Microsoft.EntityFrameworkCore;
 using ZAAL_SQL.Data;
 using ZAAL_SQL.Models;
 
+
 namespace ZAAL_SQL.Pages.LFMAS.LFM
 {
     public class IndexModel : PageModel
     {
+        public IList<MovieModel> Movie { get; set; } = new List<MovieModel>();
+        public IList<MovieModel> movies { get; set; } = new List<MovieModel>();
+
         private readonly MoviesDataContext _context;
 
         public IndexModel(MoviesDataContext context)
@@ -16,11 +20,10 @@ namespace ZAAL_SQL.Pages.LFMAS.LFM
             _context = context;
         }
 
-        public IList<MovieModel> movies { get; set; } = default!;
-
         public async Task<IActionResult> OnGetAsync()
         {
             movies = await _context.MovieModel.ToListAsync();
+            Movie = movies.ToList();
             return Page();
         }
 
@@ -28,7 +31,19 @@ namespace ZAAL_SQL.Pages.LFMAS.LFM
         {
             if (true)
             {
-                TempData["Message"] = $"movie added to your list!";
+                TempData["Message"] = $"added to your movie list!";
+                var movie = new MovieModel
+                {
+                    Titel = Request.Form["Title"].ToString(),
+                    Date = int.TryParse(Request.Form["Date"], out int date) ? date : null,
+                    Genre = Request.Form["Genre"].ToString(),
+                    Restricting_age = int.TryParse(Request.Form["Restricting_age"], out int age) ? age : null,
+                    Rating = int.TryParse(Request.Form["Rating"], out int rating) ? rating : null,
+                    Platform = Request.Form["Platform"].ToString()
+                };
+                _context.MovieModel.Add(movie);
+                await _context.SaveChangesAsync();
+
             }
             else
             {
